e02b7c82897d984f8c159bd53336de2d3fe67a6a
Matin Tavacoli <mtavacoli33@gmail.com>
2023-05-09 14:11:37 -0500

    turn in updates, api key update

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 $(document).ready(function () {
-  //search button feature
   $("#search-button").on("click", function () {
-    //get value in input search-value.
     var searchTerm = $("#search-value").val();
-    //empty input field.
     $("#search-value").val("");
     weatherFunction(searchTerm);
     weatherForecast(searchTerm);
   });
 
-  //search button enter key feature.
   $("#search-button").keypress(function (event) {
     var keycode = event.keyCode ? event.keyCode : event.which;
     if (keycode === 13) {
@@ -18,25 +14,20 @@ $(document).ready(function () {
     }
   });
 
-  //pull previous searches from local storage
   var history = JSON.parse(localStorage.getItem("history")) || [];
 
-  //sets history array search to correct length
   if (history.length > 0) {
     weatherFunction(history[history.length - 1]);
   }
-  //makes a row for each element in history array(searchTerms)
   for (var i = 0; i < history.length; i++) {
     createRow(history[i]);
   }
 
-  //puts the searched cities underneath the previous searched city
   function createRow(text) {
     var listItem = $("<li>").addClass("list-group-item").text(text);
     $(".history").append(listItem);
   }
 
-  //listener for list item on click function
   $(".history").on("click", "li", function () {
     weatherFunction($(this).text());
     weatherForecast($(this).text());
@@ -44,7 +35,7 @@ $(document).ready(function () {
 });
 function weatherFunction(searchTerm) {
   fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&units=imperial&appid=2d7cb52ab460b7ac9664d88f88ae807d`
+    `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&units=imperial&appid=1a2eed41952ccceeacd1c0c803f76b31`
   )
     .then(function (response) {
       console.log(response);
@@ -57,7 +48,7 @@ function weatherFunction(searchTerm) {
 
 function weatherForecast(searchTerm) {
   fetch(
-    "api.openweathermap.org/data/2.5/forecast?q={city name}&appid=2d7cb52ab460b7ac9664d88f88ae807d"
+    "api.openweathermap.org/data/2.5/forecast?q={city name}&appid=1a2eed41952ccceeacd1c0c803f76b31"
   )
     .then(function (response) {
       console.log(response);
@@ -67,17 +58,3 @@ function weatherForecast(searchTerm) {
       console.log(weatherData);
     });
 }
-
-//     $.ajax({
-//       type: "GET",
-//       url: "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={7afddf5328a74db83334492be055d162}"
-
-// var APIKey = "7afddf5328a74db83334492be055d162"
-
-//     }).then
-
-//     fetch("").then(function(tacocat) {
-// let pokename = tacocat.name
-
-// return "I choose " + pokename + "!"
-//     })
